2237a96c498256495c2cd386a64e3ea79f20333f
David <dagaydevel@free.fr>
2020-02-14 09:34:53 +0100

    correction de la bourde

--- src/main/java/fr/epsi/atlas/monnaie/ServletInitializer.java
+++ src/main/java/fr/epsi/atlas/monnaie/ServletInitializer.java
@@ -3,7 +3,7 @@ package fr.epsi.atlas.monnaie;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-public class ServletInitializer {
+public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
